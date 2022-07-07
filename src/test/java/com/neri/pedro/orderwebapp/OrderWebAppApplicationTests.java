1c8c3c1bcdfc6223fa5a559390c202fdc348ebe3
pedro.neri <pedro.neri@cidadania.gov.br>
2022-07-07 08:55:30 -0300

    Project created

---
+++ src/test/java/com/neri/pedro/orderwebapp/OrderWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neri.pedro.orderwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
